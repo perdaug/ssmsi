4c847854188941b0e50b8856e0116cd0ffe18567
perdaugmzas <2019828p@student.gla.ac.uk>
2017-01-20 11:38:51 +0000

    Fixing the thetas update.

--- code/ldas/gibbs_lda.py
+++ code/ldas/gibbs_lda.py
@@ -1,93 +1,99 @@
 import os
 import pandas as pd
 import numpy as np
-
-HOME_PATH = os.path.expanduser('~')
 np.set_printoptions(edgeitems=5)
 
 
-class Gibbs_LDA(object):
+class LDA_Gibbs(object):
 
-    def __init__(self, K, iter_count):
-        self.iter_count = iter_count
-        self.K = K
-        self.thetas_ = []
+	def __init__(self, K, iter_count):
+		self.iter_count = iter_count
+		self.K = K
+		self.thetas = []
 
-    def fit(self, corpus):
-        self._fit(corpus)
+	def fit(self, corpus):
+		self._fit(corpus)
 
-    def dump_thetas(self, path):
-        pickle = np.array(self.thetas_)
-        pickle.dump(path)
+	def dump_thetas(self, path):
+		pickle = np.array(self.thetas)
+		pickle.dump(path)
 
-    def _fit(self, corpus):
-        self._initialise(corpus)
+	def _fit(self, corpus):
+		self._initialise(corpus)
 
-        for it in range(self.iter_count):
-            print("Iteration %s:" % it) 
-            self._sample_topics(corpus)
-            
-            self._store_theta()
-            print("Current theta:")
-            print(self.thetas_[it]) 
+		for it in range(self.iter_count):
+			print("Iteration %s:" % it)
+			self._sample_topics(corpus)
+			self._store_theta()
+			print("Current theta:")
+			print(self.thetas[it])
 
-    def _initialise(self, corpus):
-        self.D, self.W = corpus.shape
-        self._total_frequencies = corpus.sum()
-        self.alpha = 1. / self.K
-        self.beta = 1. / self.W
+	def _initialise(self, corpus):
+		self.D, self.W = corpus.shape
+		self._total_frequencies = corpus.sum()
+		self.alpha = 1. / self.K
+		self.beta = 1. / self.W
 
-        self._zw_counts = np.zeros((self.K, self.W), dtype=np.int)
-        self._dz_counts = np.zeros((self.D, self.K), dtype=np.int)
-        self._z_counts = np.zeros(self.K, dtype=np.int)
-        self._z_history = []
+		self._zw_counts = np.zeros((self.K, self.W), dtype=np.int)
+		self._dz_counts = np.zeros((self.D, self.K), dtype=np.int)
+		self._z_counts = np.zeros(self.K, dtype=np.int)
+		self._z_history = []
 
-        for d, doc in enumerate(corpus):
-            self._z_history.append([])
-            for w, word in enumerate(doc):
-                self._z_history[d].append([])
-                word_frequency = int(word)
-                for _ in range(word_frequency):
-                    z = np.random.randint(self.K)
-                    self._z_history[d][w].append(z)
-                    self._dz_counts[d, z] += 1
-                    self._zw_counts[z, w] += 1
-                    self._z_counts[z] += 1
+		for d, doc in enumerate(corpus):
+			self._z_history.append([])
+			for w, word in enumerate(doc):
+				self._z_history[d].append([])
+				word_frequency = int(word)
+				for _ in range(word_frequency):
+					z = np.random.randint(self.K)
+					self._z_history[d][w].append(z)
+					self._dz_counts[d, z] += 1
+					self._zw_counts[z, w] += 1
+					self._z_counts[z] += 1
 
-    def _sample_topics(self, corpus):
-        for d, doc in enumerate(corpus):
-            for w, _ in enumerate(doc):
-                for it, z in enumerate(self._z_history[d][w]):
-                    self._dz_counts[d, z] -= 1
-                    self._zw_counts[z, w] -= 1
-                    self._z_counts[z] -= 1
+	def _sample_topics(self, corpus):
+		for d, doc in enumerate(corpus):
+			for w, _ in enumerate(doc):
+				for it, z in enumerate(self._z_history[d][w]):
+					self._dz_counts[d, z] -= 1
+					self._zw_counts[z, w] -= 1
+					self._z_counts[z] -= 1
 
-                    # The second denominator from eq. 5 (Griffiths and Stevyers 2004) is omitted because it is a constant and we normalise the vector p_topic anyway.
-                    p_topic = ((1.0 * self._zw_counts[:, w] + self.beta) / \
-                        (1.0 * self._z_counts + self.W * self.beta)) * \
-                        ((self._dz_counts[d, :] + self.alpha))
-                    p_topic = p_topic / p_topic.sum()
-                    z_new = np.random.choice(self.K, p=p_topic)
+					# The second denominator from eq. 5 (Griffiths and Stevyers
+					# 2004) is omitted because it is a constant and we normalise
+					# the vector p_topic anyway.
+					p_topic = ((1.0 * self._zw_counts[:, w] + self.beta) / \
+						(1.0 * self._z_counts + self.W * self.beta)) * \
+						((self._dz_counts[d, :] + self.alpha))
+					p_topic = p_topic / p_topic.sum()
+					z_new = np.random.choice(self.K, p=p_topic)
 
-                    self._z_history[d][w][it] = z_new
-                    self._dz_counts[d, z_new] += 1
-                    self._zw_counts[z_new, w] += 1
-                    self._z_counts[z_new] += 1
+					self._z_history[d][w][it] = z_new
+					self._dz_counts[d, z_new] += 1
+					self._zw_counts[z_new, w] += 1
+					self._z_counts[z_new] += 1
 
-    def _store_theta(self):
-        theta = self._dz_counts + self.alpha
-        theta /= np.sum(theta, axis=1)[:, np.newaxis]
-        theta = np.random.dirichlet(theta)
-        self.thetas_.append(theta)
+	def _store_theta(self):
+		thetas_prev = (self._dz_counts + self.alpha)
+		thetas_p_norm = thetas_prev \
+										/ np.sum(thetas_prev, axis=1)[:, np.newaxis]
 
+		thetas_current = np.zeros(shape=thetas_prev.shape)
+		for doc_idx, theta_p_n in enumerate(thetas_p_norm):
+			theta_current = np.random.dirichlet(theta_p_n)
+			thetas_current[doc_idx, :] = theta_current
+		self.thetas.append(np.array(thetas_current))
+
+
+HOME_PATH = os.path.expanduser('~') + '/Projects/ssmsi/'
+DATA_PATH = HOME_PATH + 'pickles/corpora/a-to-h/'
+OUT_PATH = HOME_PATH + 'pickles/corpora/results/lda_gibbs/'
 
 def main():
-    corpus = pd.read_pickle(HOME_PATH + '/Projects/tminm/heavy_pickles/preprocessed_corpus.pickle')
-
-    gibbs_lda = Gibbs_LDA(K=10, iter_count=50)
-    gibbs_lda.fit(corpus)
-    gibbs_lda.dump_thetas(HOME_PATH + '/Projects/tminm/pickles/' + 'document_topic_probabilities_ari_gibbs.pickle')
-
+	lda_gibbs = LDA_Gibbs(K=10, iter_count=10)
+	corpus = pd.read_pickle(DATA_PATH + 'a-to-h_corpus_r.pickle')
+	lda_gibbs.fit(corpus)
+	lda_gibbs.dump_thetas(OUT_PATH + 'gibbs_thetas.pickle')
 
 if __name__ == '__main__':
-    main()
+	main()
